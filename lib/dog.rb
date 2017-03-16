53026e3b70d2bc890cc3a560daeb12a8c155b4c4
ethanryan <ethanryan@gmail.com>
2017-03-16 00:15:17 -0400

    instance variables

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+
+  def name
+    @this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+# Why did it work? Inside the `#name=` method,
+# we set the value of `@this_dogs_name` equal to
+# whatever string is passed in as an argument.
+# Then, we are able to call on that same instance variable
+# in a totally separate method, the `#name` method.
